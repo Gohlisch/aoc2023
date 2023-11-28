96ae100b9f19d0acd2219b244cbabd7153e2ca01
timgohlisch <tim.gohlisch001@stud.fh-dortmund.de>
2023-11-29 00:52:34 +0100

    refactor second test run

--- test_03_15/logic.ts
+++ test_03_15/logic.ts
@@ -1,44 +1,45 @@
-export function calcHousesVisited(input: string) {
+export function calcHousesVisited(input: string): number {
     const housesVisited = new Set<string>();
-    const currentPosition = [0,0];
+    const currentPosition: [number, number]  = [0,0];
 
-    housesVisited.add(`${currentPosition[0]},${currentPosition[1]}`);
+    housesVisited.add(pairAsString(currentPosition));
     for(const direction of input.split("")) {
-
-        switch (direction) {
-            case ">": currentPosition[0] += 1; break;
-            case "<": currentPosition[0] -= 1; break;
-            case "^": currentPosition[1] += 1; break;
-            case "v": currentPosition[1] -= 1; break;
-        }
-
-        housesVisited.add(`${currentPosition[0]},${currentPosition[1]}`);
+        moveInDirection(currentPosition, direction as Direction);
+        housesVisited.add(pairAsString(currentPosition));
     }
 
     return housesVisited.size;
 }
 
-export function calcHousesVisitedWithRobotSanta(input: string) {
-    const realPositions = new Set<string>();
-    const realPosition = [0,0];
-    const robotPosition = [0,0];
+export function calcHousesVisitedWithRobotSanta(input: string): number {
+    const positions = new Set<string>();
+    const realPosition: [number, number] = [0,0];
+    const robotPosition: [number, number]  = [0,0];
 
     let realSantaTurn = true;
-    realPositions.add(`${realPosition[0]},${realPosition[1]}`);
+    positions.add(pairAsString(realPosition));
     for(const direction of input.split("")) {
+        const toBeMoved = realSantaTurn ? realPosition : robotPosition;
 
-        switch (direction) {
-            case ">": if(realSantaTurn) realPosition[0] += 1; else robotPosition[0] += 1; break;
-            case "<": if(realSantaTurn) realPosition[0] -= 1; else robotPosition[0] -= 1; break;
-            case "^": if(realSantaTurn) realPosition[1] += 1; else robotPosition[1] += 1; break;
-            case "v": if(realSantaTurn) realPosition[1] -= 1; else robotPosition[1] -= 1; break;
-        }
-
-        if(realSantaTurn) realPositions.add(`${realPosition[0]},${realPosition[1]}`);
-        else realPositions.add(`${robotPosition[0]},${robotPosition[1]}`);
-
+        moveInDirection(toBeMoved, direction as Direction);
+        positions.add(pairAsString(toBeMoved));
         realSantaTurn = !realSantaTurn;
     }
 
-    return realPositions.size;
+    return positions.size;
+}
+
+type Direction = "<"|">"|"^"|"v";
+
+function pairAsString(pair: [number, number]): string {
+    return `${pair[0]},${pair[1]}`;
+}
+
+function moveInDirection( position: [number, number], direction: Direction): void {
+    switch (direction) {
+        case ">": position[0] += 1; break;
+        case "<": position[0] -= 1; break;
+        case "^": position[1] += 1; break;
+        case "v": position[1] -= 1; break;
+    }
 }
